555e3148667d806a8a21d2ccb0fd423b4f53f7f2
Yuri Ribeiro <yuri.maycon@rethink.dev>
2023-12-17 22:05:36 -0300

    initial structure

---
+++ Atividades/atividade-pratica-01/src/test/java/com/example/bloodDonationSchedule/BloodDonationScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bloodDonationSchedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonationScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
